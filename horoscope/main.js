2b8e2c8f1d35b4ab65fa0cd4d68bd3a35225ffd0
Venkat-18 <venkatesh18596@gmail.com>
2017-12-14 21:18:34 +0530

    Added cache feature

--- horoscope/main.js
+++ horoscope/main.js
@@ -1,4 +1,5 @@
-var request = require("request");
+var requestMain = require("request");
+var fs = require("fs");
 var utils = require("../utils.js");
 var signs = [
   "aries",
@@ -14,6 +15,26 @@ var signs = [
   "aquarius",
   "pisces"
 ];
+var request = {};
+request.get = function(url, fn) {
+  var today = new Date().getDate();
+  var fName = "horoscope/data/" + encodeURIComponent(url) + "__" + today;
+  var filePresent = fs.existsSync(fName);
+  if (filePresent) {
+    var resp = fs.readFileSync(fName).toString();
+    console.log("Cache...");
+    return fn(null, null, resp);
+  }
+  console.log("Actual...");
+  return requestMain.get(url, fn);
+};
+
+function cache(url, resp) {
+  var today = new Date().getDate();
+  var fName = "horoscope/data/" + encodeURIComponent(url) + "__" + today;
+  fs.writeFileSync(fName, resp);
+}
+
 function getToday(sign, cb) {
   sign = sign.toLowerCase();
   var index = signs.indexOf(sign) + 1;
@@ -21,21 +42,20 @@ function getToday(sign, cb) {
     cb({ result: "Invalid Sign" });
     return;
   }
-  request.get(
-    "http://astrology.dinakaran.com/teamajax.asp?cat=" + index,
-    function(err, httpRes, body) {
-      var pattern = {
-        head: "/>(.*)</h2",
-        desc: "p>(.*)</p"
-      };
-      // console.log(body);
-      var dt = new Date();
-      var result = utils.regexParser(pattern, body);
-      result.date =
-        dt.getDate() + "." + (dt.getMonth() + 1) + "." + dt.getUTCFullYear();
-      cb(result);
-    }
-  );
+  var url = "http://astrology.dinakaran.com/teamajax.asp?cat=" + index;
+  request.get(url, function(err, httpRes, body) {
+    cache(url, body);
+    var pattern = {
+      head: "/>(.*)</h2",
+      desc: "p>(.*)</p"
+    };
+    // console.log(body);
+    var dt = new Date();
+    var result = utils.regexParser(pattern, body);
+    result.date =
+      dt.getDate() + "." + (dt.getMonth() + 1) + "." + dt.getUTCFullYear();
+    cb(result);
+  });
 }
 
 function getWeek(sign, cb) {
@@ -45,21 +65,21 @@ function getWeek(sign, cb) {
     cb({ result: "Invalid Sign" });
     return;
   }
-  request.get(
-    "http://astrology.dinakaran.com/weeklypalandetail.asp?aid=5&rid=" + index,
-    function(err, httpRes, body) {
-      var pattern = {
-        head: 'main-rasibalan"><h1><a href="#">(.*?)</a',
-        desc: 'img"/>(.*)<br><br><',
-        date: 'main-rasibalan"><h1><a href="#">.*</a></h1>(.*?)<p'
-      };
-      // console.log(body);
-      var result = utils.regexParser(pattern, body);
-      result.desc = result.desc.replace(/&nbsp;/g, " ");
-      // result.date = new Date();
-      cb(result);
-    }
-  );
+  var url =
+    "http://astrology.dinakaran.com/weeklypalandetail.asp?aid=5&rid=" + index;
+  request.get(url, function(err, httpRes, body) {
+    cache(url, body);
+    var pattern = {
+      head: 'main-rasibalan"><h1><a href="#">(.*?)</a',
+      desc: 'img"/>(.*)<br><br><',
+      date: 'main-rasibalan"><h1><a href="#">.*</a></h1>(.*?)<p'
+    };
+    // console.log(body);
+    var result = utils.regexParser(pattern, body);
+    result.desc = result.desc.replace(/&nbsp;/g, " ");
+    // result.date = new Date();
+    cb(result);
+  });
 }
 
 function getMonth(sign, cb) {
@@ -69,23 +89,23 @@ function getMonth(sign, cb) {
     cb({ result: "Invalid Sign" });
     return;
   }
-  request.get(
+  var url =
     "http://astrology.dinakaran.com/tamilmathapalandetail.asp?aid=5&rid=" +
-      index,
-    function(err, httpRes, body) {
-      var pattern = {
-        head: 'main-rasibalan"><h1><a href="#">(.*?)</a',
-        desc: 'img"/>(.*?)<br><br><',
-        date: 'main-rasibalan"><h1><a href="#">.*</a></h1>(.*?)<p'
-      };
-      // console.log(body);
-      var result = utils.regexParser(pattern, body);
-      result.desc = result.desc.replace(/&nbsp;/g, " ");
-      result.desc = result.desc.replace(/<br>/g, "\n");
-      // result.date = new Date();
-      cb(result);
-    }
-  );
+    index;
+  request.get(url, function(err, httpRes, body) {
+    cache(url, body);
+    var pattern = {
+      head: 'main-rasibalan"><h1><a href="#">(.*?)</a',
+      desc: 'img"/>(.*?)<br><br><',
+      date: 'main-rasibalan"><h1><a href="#">.*</a></h1>(.*?)<p'
+    };
+    // console.log(body);
+    var result = utils.regexParser(pattern, body);
+    result.desc = result.desc.replace(/&nbsp;/g, " ");
+    result.desc = result.desc.replace(/<br>/g, "\n");
+    // result.date = new Date();
+    cb(result);
+  });
 }
 // getMonth("aries",console.log);
 
